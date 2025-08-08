59ab6ae5da46bdfa9bc2431bae646ad99b5fe4b3
3Park <lovehyunju7@naver.com>
2025-08-08 14:13:14 +0900

    example1

--- 01_First/src/test/jgpark/first/Head01/example/HelloWorld.java
+++ 01_First/src/test/jgpark/first/Head01/example/HelloWorld.java
@@ -3,5 +3,10 @@ package test.jgpark.first.Head01.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
+
+        int score = 80;
+        String res = (score >= 90) ? "A" : "B";
+
+
     }
 }
