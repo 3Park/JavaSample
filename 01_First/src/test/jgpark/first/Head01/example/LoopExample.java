017295f4a0d3329aabbdd73f96223f64b5d69d5d
3Park <lovehyunju7@naver.com>
2025-08-08 16:23:07 +0900

    ex4

--- 01_First/src/test/jgpark/first/Head01/example/LoopExample.java
+++ 01_First/src/test/jgpark/first/Head01/example/LoopExample.java
@@ -5,6 +5,8 @@ public class LoopExample {
 
         Exam1();
         Exam2();
+        Exam3();
+        Exam4();
     }
 
     private static void Exam1()
@@ -89,6 +91,23 @@ public class LoopExample {
         String[] animals = {"Cat", "Dog", "Bird","Cat", "Dog", "Bird","Cat", "Dog", "Bird","Cat", "Dog", "Bird"};
         for (String animal : animals) {
             System.out.println(animal);
+        }
+    }
+
+    private static void Exam4()
+    {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+
+            System.out.println();
         }
     }
 }
