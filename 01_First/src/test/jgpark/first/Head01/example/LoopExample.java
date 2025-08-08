3eee0361f284390e261112af449710d2f735f0e5
3Park <lovehyunju7@naver.com>
2025-08-08 15:20:15 +0900

    ex2

--- 01_First/src/test/jgpark/first/Head01/example/LoopExample.java
+++ 01_First/src/test/jgpark/first/Head01/example/LoopExample.java
@@ -18,5 +18,59 @@ public class LoopExample {
         for (int s : scores) {
             System.out.println("배열 값: " + s);
         }
+
+        int count = 0;
+
+        while (count < 3) {
+            System.out.println("count = " + count);
+            count++;
+        }
+
+        int i1 = 5;
+
+        do {
+            System.out.println("i = " + i1);
+            i1++;
+        } while (i1 < 5);
+
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) continue;
+            if (i == 4) break;
+            System.out.println(i);
+        }
+
+        Exam2();
+    }
+
+    private static void Exam2()
+    {
+        int num = 2;
+
+        if (num % 2 == 0) {
+            System.out.println("짝수");
+        } else {
+            System.out.println("홀수");
+        }
+
+        switch (num) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            default:
+                System.out.println("Other");
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("i = " + i);
+        }
+
+        int count = 0;
+        while (count < 3) {
+            System.out.println("count = " + count);
+            count++;
+        }
     }
 }
